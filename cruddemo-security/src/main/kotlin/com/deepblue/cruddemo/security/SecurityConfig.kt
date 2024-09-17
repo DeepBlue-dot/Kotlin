c452f02312b583e25fda087cd0ca8999a11c3c97
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2024-09-17 23:00:22 +0300

    crud demo

--- cruddemo-security/src/main/kotlin/com/deepblue/cruddemo/security/SecurityConfig.kt
+++ cruddemo-security/src/main/kotlin/com/deepblue/cruddemo/security/SecurityConfig.kt
@@ -7,29 +7,16 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
 
 @Configuration
 class SecurityConfig {
     @Bean
-    fun userDetail(): InMemoryUserDetailsManager {
-        val user1 = User.builder()
-            .username("jone")
-            .password("{noop}1234")
-            .roles("EMPLOYEE")
-            .build()
-        val user2 = User.builder()
-            .username("mary")
-            .password("{noop}1234")
-            .roles("EMPLOYEE", "MANAGER")
-            .build()
-        val user3 = User.builder()
-            .username("susan")
-            .password("{noop}1234")
-            .roles("EMPLOYEE", "MANAGER", "ADMIN")
-            .build()
-
-        return InMemoryUserDetailsManager(user1, user2, user3)
+    fun userDetailManager(dataSource: DataSource): UserDetailsManager {
+        return JdbcUserDetailsManager(dataSource)
     }
 
     @Bean
@@ -44,7 +31,32 @@ class SecurityConfig {
         }
         http.httpBasic(Customizer.withDefaults())
 
+        http.csrf{csrf->csrf.disable()}
+
         // Build and return the SecurityFilterChain
         return http.build()
     }
+
+    /*
+@Bean
+fun userDetail(): InMemoryUserDetailsManager {
+    val user1 = User.builder()
+        .username("jone")
+        .password("{noop}1234")
+        .roles("EMPLOYEE")
+        .build()
+    val user2 = User.builder()
+        .username("mary")
+        .password("{noop}1234")
+        .roles("EMPLOYEE", "MANAGER")
+        .build()
+    val user3 = User.builder()
+        .username("susan")
+        .password("{noop}1234")
+        .roles("EMPLOYEE", "MANAGER", "ADMIN")
+        .build()
+
+    return InMemoryUserDetailsManager(user1, user2, user3)
+}
+*/
 }
